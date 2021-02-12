36ff7bc9cc0a0fcc92a077a9dde6bca35c74e44a
stefan <stefankaraferovic@gmail.com>
2021-02-12 01:11:55 +0100

    modal dialog

---
+++ client/src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="bg-modal">
+        <div class="modal-content">
+            <div class="question">Do you really want to remove item from cart?</div>
+            <div class="buttons">
+                <div @click="close" class="button">No</div>
+                <div class="button">Confirm</div>
+            </div>
+            <div @click="close" class="close">+</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Modal',
+    methods: {
+        close() {
+            this.$emit('close');
+        },
+    },
+};
+</script>
+
+<style></style>
--- client/src/views/ShoppingCart.vue
+++ client/src/views/ShoppingCart.vue
@@ -4,26 +4,60 @@
             <div class="title">Your cart</div>
             <div v-for="item in items" :key="item._id" class="cart_item">
                 <div class="cart_item__img">
-                    <img :src="`/uploads/${item.photo || 'sneaker.jpg'}`" alt="sneaker photo" />
-                    <!-- <img src="@/assets/images/sneaker.jpg" alt="sneaker photo" /> -->
+                    <!-- <img
+                        :src="`/uploads/${item.product.photo || 'sneaker.jpg'}`"
+                        alt="sneaker photo"
+                    /> -->
+                    <img src="@/assets/images/sneaker.jpg" alt="sneaker photo" />
+                </div>
+                <div class="cart_item__details">
+                    <p>{{ item.product.name }}</p>
+                    <div>Size: {{ item.size.number }}</div>
+                </div>
+                <div class="cart_item__price">
+                    <div>{{ item.product.price }} $</div>
+                </div>
+                <div class="cart_item__price">
+                    <div>0,00%</div>
+                </div>
+                <div class="cart_item__price">
+                    <div>{{ item.product.price }} $</div>
+                </div>
+                <div class="cart_item__remove">
+                    <div @click="showModal" class="removeBtn">Remove</div>
                 </div>
             </div>
         </div>
+        <Modal v-show="isModalVisible" @close="closeModal" />
     </div>
 </template>
 
 <script>
+import Modal from '@/components/Modal';
 export default {
     name: 'ShoppingCart',
+    components: {
+        Modal,
+    },
     data() {
         return {
             items: [],
+            isModalVisible: false,
         };
+    },
+    methods: {
+        showModal() {
+            this.isModalVisible = true;
+            //todo proslediti item koji se brise na confirm
+            //todo ukupna cena
+        },
+        closeModal() {
+            this.isModalVisible = false;
+        },
     },
     created() {
         this.items = this.$store.state.cartItems;
     },
-    methods: {},
 };
 </script>
 
