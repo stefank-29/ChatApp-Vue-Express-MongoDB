fa2ef77a02c283cfdf9866125237db50cab99c71
stefank-29 <stefankaraferovic@gmail.com>
2021-03-11 22:59:38 +0100

    deploy api route

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default () => {
     return axios.create({
-        baseURL: `http://localhost:3000/api/`,
+        baseURL: `https://sneakers-shop-website.herokuapp.com/api/`,
         headers: {
             'Content-type': 'application/json',
         },
